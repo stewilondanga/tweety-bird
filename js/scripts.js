d65d845b79b7dfd3089174eec1b1f457d2d3c375
Stewart ilondanga <stilondanga@gmail.com>
2019-01-06 10:29:37 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,58 +12,58 @@ document.addEventListener("DOMContentLoaded", function() {
   this.querySelector("form").addEventListener("submit", tweet);
 });
 
-/*function ctChars() {
-	let el = this,
-		to = setTimeout(function() {
-			let len = el.value.length,
-				ct = document.querySelector("span"),
-				btn = document.querySelector("button"),
-				warnAt = 20,
-				max = ct.getAttribute("data-limit");
+function ctChars() {
+  let el = this,
+    to = setTimeout(function() {
+        let len = el.value.length,
+          ct = document.querySelector("span"),
+          btn = document.querySelector("button"),
+          warnAt = 20,
+          max = ct.getAttribute("data-limit");
 
-			// characters left
-			ct.innerHTML = max - len;
-			// warn about reaching limit
-			ct.className = len > max - warnAt ? "warn" : "";
-			btn.disabled = len == 0 ? true : false;
-			clearTimeout(to);
-		}, 1);
-}
-function tweet(e) {
-	let el = this,
-		ta = el.querySelector("textarea");
-	el.classList.add("pre-morph");
-	ta.disabled = true;
+        /*			// characters left
+        			ct.innerHTML = max - len;
+        			// warn about reaching limit
+        			ct.className = len > max - warnAt ? "warn" : "";
+        			btn.disabled = len == 0 ? true : false;
+        			clearTimeout(to);
+        		}, 1);
+        }
+        function tweet(e) {
+        	let el = this,
+        		ta = el.querySelector("textarea");
+        	el.classList.add("pre-morph");
+        	ta.disabled = true;
 
-	// bird animation
-	var tl = new TimelineMax();
-	tl
-		.to("#b1", 0.2, {
-			delay: 0.2,
-			morphSVG:"#b2",
-			ease: Linear.easeIn
-		})
-		.to("#b1", 0.25, {
-			morphSVG:"#b3",
-			ease: Linear.easeIn
-		})
-		.to("#b1", 0, {
-			delay: 0.05,
-			opacity: 0
-		});
+        	// bird animation
+        	var tl = new TimelineMax();
+        	tl
+        		.to("#b1", 0.2, {
+        			delay: 0.2,
+        			morphSVG:"#b2",
+        			ease: Linear.easeIn
+        		})
+        		.to("#b1", 0.25, {
+        			morphSVG:"#b3",
+        			ease: Linear.easeIn
+        		})
+        		.to("#b1", 0, {
+        			delay: 0.05,
+        			opacity: 0
+        		});
 
-	// reset everything
-	setTimeout(function(){
-		let ct = el.querySelector("span"),
-			btn = el.querySelector("button");
+        	// reset everything
+        	setTimeout(function(){
+        		let ct = el.querySelector("span"),
+        			btn = el.querySelector("button");
 
-		el.classList.remove("pre-morph")
-		ta.value = "";
-		ta.disabled = false;
-		ct.innerHTML = ct.getAttribute("data-limit");
-		btn.disabled = true;
-		tl.pause(0);
-	}, 3000);
+        		el.classList.remove("pre-morph")
+        		ta.value = "";
+        		ta.disabled = false;
+        		ct.innerHTML = ct.getAttribute("data-limit");
+        		btn.disabled = true;
+        		tl.pause(0);
+        	}, 3000);
 
-	e.preventDefault();
-}
+        	e.preventDefault();
+        }
